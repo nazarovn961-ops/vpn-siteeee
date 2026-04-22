c90e4726cfc983a31a22dbabd901e914a2be965c
nazarovn961-ops <nazarovn961@gmail.com>
2026-04-22 15:37:08 +0300

    Update sub.js

--- vpn-site/netlify/functions/sub.js
+++ vpn-site/netlify/functions/sub.js
@@ -1,77 +1,70 @@
 exports.handler = async function () {
-  const config = [
-    {
-      "dns": {
-        "servers": [
-          "1.1.1.1"
-        ]
-      },
-      "inbounds": [
-        {
-          "listen": "127.0.0.1",
-          "port": 10808,
-          "protocol": "socks",
-          "settings": {
-            "auth": "noauth",
-            "udp": true
-          },
-          "tag": "socks"
-        },
-        {
-          "listen": "127.0.0.1",
-          "port": 10809,
-          "protocol": "http",
-          "settings": {},
-          "tag": "http"
+  const config = {
+    "dns": {
+      "servers": ["1.1.1.1"]
+    },
+    "inbounds": [
+      {
+        "listen": "127.0.0.1",
+        "port": 10808,
+        "protocol": "socks",
+        "settings": {
+          "auth": "noauth",
+          "udp": true
         }
-      ],
-      "outbounds": [
-        {
-          "protocol": "vless",
-          "settings": {
-            "vnext": [
-              {
-                "address": "at.titun.su",
-                "port": 443,
-                "users": [
-                  {
-                    "id": "a44a6111-be40-4a96-8947-cb82fe37723b",
-                    "encryption": "none",
-                    "flow": "xtls-rprx-vision"
-                  }
-                ]
-              }
-            ]
-          },
-          "streamSettings": {
-            "network": "tcp",
-            "security": "reality",
-            "realitySettings": {
-              "serverName": "pogovorim.su",
-              "publicKey": "1vSZjvhZO01oAEH3b7eebR1qF5dLU1Dq2E7xu8pwGSs",
-              "shortId": "428ef87fd47a3a32",
-              "fingerprint": "chrome"
+      },
+      {
+        "listen": "127.0.0.1",
+        "port": 10809,
+        "protocol": "http",
+        "settings": {}
+      }
+    ],
+    "outbounds": [
+      {
+        "protocol": "vless",
+        "settings": {
+          "vnext": [
+            {
+              "address": "at.titun.su",
+              "port": 443,
+              "users": [
+                {
+                  "id": "a44a6111-be40-4a96-8947-cb82fe37723b",
+                  "encryption": "none",
+                  "flow": "xtls-rprx-vision"
+                }
+              ]
             }
-          },
-          "tag": "proxy"
+          ]
         },
-        {
-          "protocol": "freedom",
-          "tag": "direct"
-        }
-      ],
-      "routing": {
-        "domainStrategy": "IPIfNonMatch",
-        "rules": []
+        "streamSettings": {
+          "network": "tcp",
+          "security": "reality",
+          "realitySettings": {
+            "serverName": "pogovorim.su",
+            "publicKey": "1vSZjvhZO01oAEH3b7eebR1qF5dLU1Dq2E7xu8pwGSs",
+            "shortId": "428ef87fd47a3a32",
+            "fingerprint": "chrome"
+          }
+        },
+        "tag": "proxy"
       },
-      "remarks": "TEST AUSTRIA"
+      {
+        "protocol": "freedom",
+        "tag": "direct"
+      }
+    ],
+    "routing": {
+      "domainStrategy": "IPIfNonMatch",
+      "rules": []
     }
-  ];
+  };
 
   return {
     statusCode: 200,
     headers: {
-      "Content-Type": "application/json; charset=utf-8"
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(config)
   };
